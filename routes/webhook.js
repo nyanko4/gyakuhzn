c1a4976005a7527fae90fb7853925642eac698b5
nyanko4 <nyankobetsuaka@outlook.com>
2025-12-09 16:49:25 +0900

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const getchat = require("../webhook/getchat");
+const mention = require("../webhook/mention");
 
 router.post("/getchat", (req, res) => {
   getchat(req, res);
+});
+
+router.post("/mention", (req, res) => {
+  mention(req, res);
 });
 
 module.exports = router;
