2a55518f56a3a5714e5f97e0ad504cda78fdf796
nyanko4 <nyankobetsuaka@outlook.com>
2025-12-09 16:53:00 +0900

    Create mention.js

---
+++ webhook/mention.js
@@ -0,0 +1,20 @@
+const sendgyaku = require("../module/sendgyaku");
+
+async function mentionWebhook(req, res) {
+  const {
+    from_account_id: accountId,
+    room_id: roomId,
+    message_id: messageId,
+    body
+  } = req.body.webhook_event;
+  if (/\[(?:rp|返信) aid=\d+ to=\d+-\d+\]/.test(body)) {
+    return res.sendStatus(200);
+  }
+
+  if (accountId === 10788439) {
+    await sendgyaku(body, messageId, roomId, accountId);
+  }
+  res.sendStatus(500);
+}
+
+module.exports = mentionWebhook;
